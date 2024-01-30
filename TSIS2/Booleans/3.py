72731a47839f6c1bbffae952d2697b6e4f63df81
Victoria <vradke0@gmail.com>
2024-01-30 09:34:50 +0600

    booleans n operators

---
+++ TSIS2/Booleans/3.py
@@ -0,0 +1,6 @@
+print(bool("Hello"))
+print(bool(15))
+
+bool("abc")
+bool(123)
+bool(["apple", "cherry", "banana"])
---
+++ TSIS2/Booleans/5.py
@@ -0,0 +1,12 @@
+def myFunction() :
+  return True
+
+print(myFunction())
+
+def myFunction() :
+  return True
+
+if myFunction():
+  print("YES!")
+else:
+  print("NO!")
